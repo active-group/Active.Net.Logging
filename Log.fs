84685326b56710b1c600b46330a4e149972c9b5c
Andreas Bernauer <andreas.bernauer@active-group.de>
2015-11-03 09:06:51 +0100

    initial commit

---
+++ Log.fs
@@ -0,0 +1,15 @@
+﻿namespace Active.Net
+
+module Log =
+
+    let getLogger (id: string): NLog.Logger = NLog.LogManager.GetLogger(id)
+
+    let logTrace (logger: NLog.Logger) (msg: string) = logger.Trace msg
+    let logDebug (logger:NLog.Logger) (msg:string) = logger.Debug msg 
+    let logInfo (logger:NLog.Logger) (msg:string) = logger.Info msg 
+    let logWarn (logger:NLog.Logger) (msg:string) = logger.Warn msg
+    let logError (logger:NLog.Logger) (msg:string) = logger.Error msg
+    let logFatal (logger:NLog.Logger) (msg:string) = logger.Fatal msg
+
+
+
---
+++ Logging.fs
@@ -0,0 +1,36 @@
+﻿namespace Active.Net
+
+module Logging =
+    open NLog
+    open NLog.Config
+    open NLog.Targets
+
+
+    let makeConfig (loggingPath: string) (level: LogLevel) = 
+        let config = new LoggingConfiguration()
+
+        let defaultFileTarget = new FileTarget()
+
+        config.AddTarget("defaultLogging", defaultFileTarget)
+
+        defaultFileTarget.Layout <- new Layouts.SimpleLayout(@"${date:format=yyyy-MM-dd HH\:mm\:ss} ${logger} ${level}: ${message}")
+        defaultFileTarget.FileName <- new Layouts.SimpleLayout(loggingPath + "Lokalisierung.log")
+                
+        // Store the last 7 days, use 1 file per day
+        defaultFileTarget.ArchiveFileName <- new Layouts.SimpleLayout(loggingPath + "Lokalisierung.{#}.log")
+        defaultFileTarget.ArchiveEvery <- FileArchivePeriod.Day
+        defaultFileTarget.ArchiveNumbering <- ArchiveNumberingMode.Date
+        defaultFileTarget.ArchiveDateFormat <- "yyyy-MM-dd"
+        defaultFileTarget.MaxArchiveFiles <- 7
+
+        let ruleInfo = new LoggingRule("*", level, defaultFileTarget)
+        ruleInfo.Final <- true
+        config.LoggingRules.Add(ruleInfo)
+
+        config
+
+    // Initializes the logging with our specific configurations
+    let initLogging (loggingPath: string) (level: LogLevel) = 
+        LogManager.Configuration <- makeConfig loggingPath level
+        NLog.LogManager.ReconfigExistingLoggers()
+
